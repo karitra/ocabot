e538c6533151b61fc9239dff3a2c0520482b3238
karitra <karapuz@yandex-team.ru>
2021-12-01 12:12:49 +0300

    Defaults for check timeout

--- bin/config.ml
+++ bin/config.ml
@@ -19,7 +19,7 @@ let command exec =
         urls = anon (non_empty_sequence_as_list ("urls" %: string))
         and endpoint = flag "--telegram-url" (required string) ~doc:"telegram bot url"
         and chat_id = flag "--telegram-chat" (required int) ~doc:"telegram chat id"
-        and check_timeout_seconds = flag "--check-timeout" (required int) ~doc:"single url check timeout"
+        and check_timeout_seconds = flag "--check-timeout" (optional_with_default 15 int)) ~doc:"single url check timeout"
         and parallel_ping = flag "--parallel-ping" no_arg ~doc:"run pinger in parralel mode"
       in
       let telegram_config = {chat_id; endpoint} in
